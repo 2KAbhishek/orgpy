9511ca3413454311cf907d58dd4d55b865a10ccc
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-07 19:46:51 +0530

    Add dir_map for Word

--- orgy.py
+++ orgy.py
@@ -5,3 +5,7 @@ import os
 # Directory mapping, maps file extensions to directories
 dir_map = {'.txt': 'Docs', '.pdf': 'Docs' + os.sep + 'PDF'}
 
+# Word files
+dir_map.update(dict.fromkeys(
+    ['.doc', '.docx', '.odt', 'rtf'], 'Docs' + os.sep + 'Word'))
+
